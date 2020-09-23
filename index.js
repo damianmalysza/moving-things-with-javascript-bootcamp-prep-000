bb00f3fc44f4edfde0e12a2934ecb6e9d1f3e6be
Damian Malysza <damian.malysza@gmail.com>
2020-09-23 01:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,13 @@ function moveDodgerRight(){
   var rightNumbers=dodger.style.right.replace("px","")
   var right = parseInt(rightNumbers,10)
   
+  if (right<400){
+    dodger.style.right=`${right + 1}px`
+  }
 }
+
+document.addEventListener("keydown",function(e){
+  if (e.which===37){
+    moveDodgerRight()
+  }
+})

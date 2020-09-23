0e88139fa778f3762f565820f5dcbd7ff94908c6
Damian Malysza <damian.malysza@gmail.com>
2020-09-23 01:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function moveDodgerRight(){
   var rightNumbers=dodger.style.left.replace("px","")
   var right = parseInt(rightNumbers,10)
   
-  if (right<300){
+  if (right<350){
     dodger.style.left=`${right + 1}px`
   }
 }
